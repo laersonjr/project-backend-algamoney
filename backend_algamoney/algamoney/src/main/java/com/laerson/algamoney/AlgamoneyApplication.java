962056faa65f0f8e91a94aa1c637cd7312d9e70f
laersonjr <laersonjr@gmail.com>
2023-07-16 13:09:21 -0300

    Iniciando projeto algamoney

---
+++ backend_algamoney/algamoney/src/main/java/com/laerson/algamoney/AlgamoneyApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.algamoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgamoneyApplication.class, args);
+    }
+
+}
---
+++ backend_algamoney/algamoney/src/test/java/com/laerson/algamoney/AlgamoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.algamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
