49787b78ff613f5536441bbf4d48ff9ea2042d92
laersonjr <laersonjr@gmail.com>
2023-07-16 15:59:30 -0300

    model, repository e resource categoria

---
+++ backend_algamoney/algamoney/src/main/java/com/laerson/algamoney/model/Categoria.java
@@ -0,0 +1,45 @@
+package com.laerson.algamoney.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codigo", nullable = false)
+    private Long codigo;
+
+    private String nome;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Categoria categoria)) return false;
+        return Objects.equals(getCodigo(), categoria.getCodigo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+}
