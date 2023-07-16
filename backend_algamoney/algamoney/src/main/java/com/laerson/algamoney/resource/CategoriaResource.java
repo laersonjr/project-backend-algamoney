f14c1a60cfeac56f073dea95a81a52938aa24d6f
laersonjr <laersonjr@gmail.com>
2023-07-16 16:58:14 -0300

    Removendo imports não utilizados

--- backend_algamoney/algamoney/src/main/java/com/laerson/algamoney/resource/CategoriaResource.java
+++ backend_algamoney/algamoney/src/main/java/com/laerson/algamoney/resource/CategoriaResource.java
@@ -3,15 +3,12 @@ package com.laerson.algamoney.resource;
 import com.laerson.algamoney.model.Categoria;
 import com.laerson.algamoney.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("categorias")
@@ -26,7 +23,7 @@ public class CategoriaResource {
     }
 
     @PostMapping
-    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria){
         Categoria categoriaSalva = categoriaRepository.save(categoria);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
         // response.setHeader("Location", uri.toASCIIString());
